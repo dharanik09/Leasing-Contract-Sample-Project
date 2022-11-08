89741219f88c6efa1cc341772ec9441ea1711a0b
Dharani <dharan.cse09@gmail.com>
2022-11-08 08:30:44 +0100

    Update V1__Initial.sql

--- backend-amg/src/main/resources/db/migration/V1__Initial.sql
+++ backend-amg/src/main/resources/db/migration/V1__Initial.sql
@@ -1,16 +1,33 @@
 CREATE TABLE `customer` (
-  `id` int NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `first_name` varchar(45) DEFAULT NULL,
   `last_name` varchar(45) DEFAULT NULL,
   `birthdate` date DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB AUTO_INCREMENT=68 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `amg`.`vehicle` (
-  `id` INT NOT NULL,
-  `brand` VARCHAR(45) NULL,
-  `model` VARCHAR(45) NULL,
-  `model_year` YEAR(4) NULL,
-  `vehicle_identification_number` VARCHAR(45) NULL,
-  `price` DECIMAL(10,2) NULL,
-  PRIMARY KEY (`id`));
+
+CREATE TABLE `leasing_contract` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `contract_number` int DEFAULT NULL,
+  `monthly_rate` decimal(10,2) DEFAULT NULL,
+  `customer_id` int DEFAULT NULL,
+  `vehicle_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `vehicle_id_UNIQUE` (`vehicle_id`),
+  KEY `customer_id_idx` (`customer_id`),
+  KEY `vehicle_id_idx` (`vehicle_id`),
+  CONSTRAINT `customer_id` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`),
+  CONSTRAINT `vehicle_id` FOREIGN KEY (`vehicle_id`) REFERENCES `vehicle` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=70 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE `vehicle` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `brand` varchar(45) DEFAULT NULL,
+  `model` varchar(45) DEFAULT NULL,
+  `model_year` year DEFAULT NULL,
+  `vehicle_identification_number` varchar(45) DEFAULT NULL,
+  `price` decimal(10,3) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
